28028a89f11c5834a9060e479f7723e9100a9b4c
root <root@ip-172-31-83-175.ec2.internal>
2025-04-18 12:33:14 +0000

    Fix : added to content in web page

---
+++ static/game.js
@@ -0,0 +1,83 @@
+const cells = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+const winningMessage = document.getElementById('winningMessage');
+const winningMessageText = document.getElementById('winningMessageText');
+const restartButton = document.getElementById('restartButton');
+
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+let oTurn;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cells.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessage.classList.remove('show');
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageText.innerText = 'Draw!';
+    } else {
+        winningMessageText.innerText = `${oTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMessage.classList.add('show');
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
